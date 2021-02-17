99e980f1d6434602b4514a334d018acc6efaccdc
Chris Aragon <caragon.tpp@gmail.com>
2021-02-17 14:35:06 -0600

    finished

---
+++ server/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const authCtrl = require('./authController')
+const treasureCtrl = require('./treasureController')
+const auth = require('./middleware/authMiddleware')
+
+const PORT = 4000
+
+const app = express()
+const {CONNECTION_STRING, SESSION_SECRET} = process.env
+
+app.use(express.json())
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log('db connected')
+})
+
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET
+    })
+)
+
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout)
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
